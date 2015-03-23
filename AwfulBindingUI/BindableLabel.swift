6c69f8cd2fa84ddbc794b62acec412fc89305150
Zach Smith <Zachary.Clay.Smith@gmail.com>
2015-03-23 16:45:54 -0700

    rolling back BindableLabel change due to generics incompatibilities.

--- AwfulBindingUI/BindableLabel.swift
+++ AwfulBindingUI/BindableLabel.swift
@@ -3,9 +3,9 @@ import UIKit
 import AwfulBinding
 
 public class BindableLabel:UILabel{
-    private var _bindableValue:BindableValue<Any>?
+    private var _bindableValue:BindableValue<String>?
     
-    public var bindableValue:BindableValue<Any>?{
+    public var bindableValue:BindableValue<String>?{
         get{
             return _bindableValue
         }
@@ -19,8 +19,8 @@ public class BindableLabel:UILabel{
         }
     }
     
-    private func valueChanged(newValue:Any){
-        self.text = toString(newValue)
+    private func valueChanged(newValue:String){
+        self.text = newValue
     }
     
     deinit{
